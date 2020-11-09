cd30462d4a03af0768881f606e43403bc6ab34dc
Younes Mounnadi <61013634+MounnadiYounes@users.noreply.github.com>
2020-11-09 17:19:26 +0100

    v2.0

---
+++ src/main/java/domain/db/util/Secret.java
@@ -0,0 +1,11 @@
+package domain.db.util;
+
+import java.util.Properties;
+
+public class Secret extends Credentials {
+
+    public static void setPass(Properties properties) {
+        properties.setProperty("user", "local_r0786792");
+        properties.setProperty("password", "=èL3K)T§§XWp");
+    }
+}
